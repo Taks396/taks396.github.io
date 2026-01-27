55d3ac0d52797183bdbb1e2bf88d5546180fa721
Taks396 <jtakatz@outlook.com>
2026-01-27 19:38:29 +0000

    Dark mode added to Cooking Recipes. Does not persist between links.

--- Scripts/dark-mode.js
+++ Scripts/dark-mode.js
@@ -1,6 +1,6 @@
 // function getChildren(x) { return x.children.length > 0 ? [...x.children].map(y => getChildren(y)) : x }
 
-function myFunction() {
+function darkModeToggle() {
    var text = document.querySelector(".page");
    var children = text.querySelectorAll("*");
    var toggle = text.getElementsByClassName("dark-toggle")[0];
@@ -10,7 +10,7 @@ function myFunction() {
       toggle.style.filter = "invert(1)";
       text.style.backgroundColor = "#616161";
       children.forEach(descendant => {
-         descendant.style.color = "#a5a5a5";
+         descendant.style.color = "#b1b1b1";
          if (descendant.classList.contains("header")) {
             descendant.style.backgroundColor = "#222222";
          } else if (descendant.classList.contains("sidebar")) {
@@ -18,7 +18,7 @@ function myFunction() {
          } else if (descendant.classList.contains("content")) {
             descendant.style.backgroundColor = "#333333";
          } else if (descendant.tagName == "HR") {
-            descendant.style.borderColor = "#a5a5a5";
+            descendant.style.borderColor = "#b1b1b1";
          }
       });
    } else {
@@ -39,53 +39,4 @@ function myFunction() {
          }
       });
    }
-
-   
-
-   // var element = document.getElementsByClassName("dark-toggle")[0];
-   // var head = document.getElementsByClassName("header")[0];
-   // var content = document.getElementsByClassName("content")[0];
-   // var sidebar = document.getElementsByClassName("sidebar")[0];
-
-   // // Set "Light Mode" Background Colors
-   // console.log(element.style.filter)
-   // if(element.style.filter == "invert(1)") {
-   //    console.log("Default")
-   //    element.style.filter = "none"
-
-   //    element.style.backgroundColor = "#008cff"
-   //    head.style.backgroundColor = "#008cff"
-   //    content.style.backgroundColor = "#80ffdb"
-   //    sidebar.style.backgroundColor = "#80ffdb"
-
-   //    for (const child of head.children) {
-   //       child.style.color = "#000000"
-   //    }
-   //    for (const child of content.children) {
-   //       child.style.color = "#000000"
-   //    }
-   //    for (const child of sidebar.children) {
-   //       child.style.color = "#000000"
-   //    }
-
-   // // Set "Dark Mode" Background Colors
-   // } else {
-   //    console.log("Dark")
-   //    element.style.filter = "invert(1)"
-
-   //    element.style.backgroundColor = "#ffffff"
-   //    head.style.backgroundColor = "#222222"
-   //    content.style.backgroundColor = "#333333"
-   //    sidebar.style.backgroundColor = "#333333"
-      
-   //    for (const child of head.children) {
-   //       child.style.color = "#9c9c9c"
-   //    }
-   //    for (const child of content.children) {
-   //       child.style.color = "#9c9c9c"
-   //    }
-   //    for (const child of sidebar.children) {
-   //       child.style.color = "#9c9c9c"
-   //    }
-   // }
 }
